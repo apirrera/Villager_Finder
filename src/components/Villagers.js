421c559bebf67a825781e4ecd6d2630fe539c0c0
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2022-01-07 11:00:50 -0800

    nav bar

--- src/components/Villagers.js
+++ src/components/Villagers.js
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-function Villagers(){
-  const [villagers, setVillagers] = useState([]);
+function Villagers() {
+	const [villagers, setVillagers] = useState([]);
 
-  useEffect(() => {
-    fetch("https://acnhapi.com/v1/villagers")
-      .then(response => response.json())
-      .then((data) => setVillagers(data))
-  })
- //check how to traverse through obj and list villagers images first
-  return(
-    <div>
-      
-    </div>
-  );
+	useEffect(() => {
+		fetch("https://acnhapi.com/v1a/villagers")
+			.then((response) => response.json())
+			.then((response) => {
+				setVillagers(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	const list = villagers.map((villager) => <div><img src={villager.image_uri} alt="" /> <p>{villager.name["name-USen"]}</p> </div>);
+
+	return (
+		<div>
+			this is villagers page <br></br>
+			{list}
+		</div>
+	);
 }
 
 export default Villagers;
--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "./Home";
+import Villagers from "../components/Villagers";
 
 function Routes(){
 
@@ -8,6 +9,9 @@ function Routes(){
     <Switch>
       <Route exact path="/">
       <Home />
+      </Route>
+      <Route path="/villagers">
+        <Villagers />
       </Route>
     </Switch>
   )
